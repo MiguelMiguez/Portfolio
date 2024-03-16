5ea3627796152dab965569ed945202bde5122570
Miguel Miguez <miguelmiguezangel@gmail.com>
2024-03-16 17:34:28 -0300

    Update

--- src/components/IntroPerfl/IntroPerfil.jsx
+++ src/components/IntroPerfl/IntroPerfil.jsx
@@ -13,7 +13,7 @@ const IntroPerfil = () => {
             <h1 className='NameProfile'>{resumeData.basics.name}</h1>
             <p className='TextProfile'>{resumeData.basics.summary}</p>
             <div className='ContainerLocation'>
-                <a href="https://maps.app.goo.gl/Fs9TkRmV8fsE1jT88" target='blank' >Burzaco, Buenos Aires, ARG</a>
+                <a href="https://maps.app.goo.gl/Fs9TkRmV8fsE1jT88" target='blank' ><p className='Text'>Burzaco, Buenos Aires, ARG</p></a>
             </div>
             <div className='ContainerSocialMedia'>
                 <a href="https://www.linkedin.com/in/miguelmiguez/" target='blank'><p className='Text'>LinkedIn: Miguel Miguez</p></a>
