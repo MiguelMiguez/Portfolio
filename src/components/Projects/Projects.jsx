69ac7e10b7895373be169f449e2010c46bbb43ba
Miguel Miguez <miguelmiguezangel@gmail.com>
2024-03-16 15:04:27 -0300

    update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,24 +3,28 @@ import { useMyContext } from '../MyContext/MyContext'
 import "./Projects.css"
 
 const Projects = () => {
-    const{projects} = useMyContext();
+    const { projects } = useMyContext();
 
-  return (
-    <div className='Projects'>
-        <h2 className='Tittle'>Proyectos</h2>
-        <div className='ContainerProjects'>
-            {projects.map((pro)=>(
-                <div className='Plan'>
-                    <a href={pro.url}>
-                        <p>{pro.name}</p>
-                        <p>{pro.description}</p>
-                        <p>{pro.tecnology}</p>
-                    </a>
-                </div>
-            ))}
+    return (
+        <div className='Projects'>
+            <h2 className='Tittle'>Proyectos</h2>
+            <div className='ContainerProjects'>
+                {projects.map((pro, index) => (
+                    <div className='Plan' key={index}>
+                        <a href={pro.url} target='blank' ><p className='ProjectName'>{pro.name}</p></a>
+                        <p className='Text'>{pro.description}</p>
+                        <div className='ContainterTech'>
+                            {pro.tecnology.map((tech, techIndex) => (
+                                <div className='Tech' key={techIndex}>
+                                    <p className='Text'>{tech.name}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Projects
