55cf7cc893ab36f880fab9c5d0a74ee63539d82f
Miguel Miguez <miguelmiguezangel@gmail.com>
2024-07-22 11:12:13 -0300

    Update visualizacion
    -Se retiro la foto de perfil y se acomodaron los textos

--- src/components/IntroPerfl/IntroPerfil.jsx
+++ src/components/IntroPerfl/IntroPerfil.jsx
@@ -12,16 +12,8 @@ const IntroPerfil = () => {
         <div className='ContainerInfoPerfil'>
             <h1 className='NameProfile'>{resumeData.basics.name}</h1>
             <p className='TextProfile'>{resumeData.basics.summary}</p>
-            <div className='ContainerLocation'>
-                <a href="https://maps.app.goo.gl/Fs9TkRmV8fsE1jT88" target='blank' ><p className='Text'>Burzaco, Buenos Aires, ARG</p></a>
-            </div>
-            <div className='ContainerSocialMedia'>
-                <a href="https://www.linkedin.com/in/miguelmiguez/" target='blank'><p className='Text'>LinkedIn: Miguel Miguez</p></a>
-                <a href="https://github.com/MiguelMiguez" target='blank'><p className='Text'>GitHub: MiguelMiguez </p></a>
-                <a href=""><p className='Text'></p></a>
-            </div>
         </div>
-        <div className='ContainerPhoto'></div>
+       
     </div>
   )
 }
