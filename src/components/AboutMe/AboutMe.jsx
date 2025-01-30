55b65ea137d842402d81e257877219279ee590a6
Miguel Miguez <miguelmiguezangel@gmail.com>
2025-01-30 10:02:15 -0300

    Update AboutMe.jsx

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
     <div className='AboutMe'>
         <h2 className='Tittle'>Sobre mí</h2>
         <p className='Text'>
-        Profesional de IT con 7 años de experiencia en soporte y análisis de aplicaciones. Especializado en bases de datos SQL Oracle, sistemas contables e impositivos, SAP y gestión de incidentes con Jira. Cuento con conocimientos en desarrollo Front-End, lo que me brinda una visión integral para la implementación de soluciones tecnológicas. Busco seguir creciendo en análisis de aplicaciones y desarrollo, aportando valor con mi experiencia y capacidad de resolución.
+        Profesional de IT con 7 años de experiencia en soporte y análisis de aplicaciones, con enfoque en bases de datos SQL Oracle, sistemas contables e impositivos, SAP y gestión de incidentes. Cuento con conocimientos en desarrollo Front-End, lo que me permite aportar una visión más amplia en la implementación de soluciones tecnológicas. Busco seguir creciendo en análisis y soporte de aplicaciones, aportando mi experiencia técnica y capacidad de resolución.
         </p>
     </div>
   )
